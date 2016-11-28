e5cf8b4871f4cb3962072051715987eb06241a02
Xavier Lau <xavierlau1@gmail.com>
2016-11-27 17:35:18 -0800

    Remove formfields helper and fix minor typo.

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -40,7 +40,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
   |  $autoload['packages'] = array(APPPATH.'third_party', '/usr/local/shared');
   |
  */
-$autoload['packages'] = array(APPPATH.'third_party/restful/);
+$autoload['packages'] = array(APPPATH.'third_party/restful/');
 
 /*
   | -------------------------------------------------------------------
@@ -90,7 +90,7 @@ $autoload['drivers'] = array();
   |
   |	$autoload['helper'] = array('url', 'file');
  */
-$autoload['helper'] = array('url', 'formfields');
+$autoload['helper'] = array('url');
 
 /*
   | -------------------------------------------------------------------
