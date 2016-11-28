b1b68522735fe3a1d6064542f866c377e3bcc3a1
Connor Phalen <Phalen.Connor@hotmail.co.uk>
2016-11-27 16:13:22 -0800

    Added the backend logic to take in HTTP requests and return JSON data for the menu items.
    Signed-off-by: Connor Phalen <Phalen.Connor@hotmail.co.uk>

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -21,13 +21,15 @@ class Application extends CI_Controller {
 
 		//  Set basic view parameters
 		$this->data = array();
-		$this->data['pagetitle'] = "Jim's Joint";
+		$this->data['pagetitle'] = "Lab09 Backend";
 		$this->data['ci_version'] = (ENVIRONMENT === 'development') ? 'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '';
                 
+                /*
                 //  Get the user role
 		$this->data['userrole'] = $this->session->userdata('userrole');
 		if ($this->data['userrole'] == NULL) 
                     $this->data['userrole'] = '?';
+                */
 	}
 
 	/**
@@ -40,10 +42,12 @@ class Application extends CI_Controller {
 		if (!isset($this->data['content']))
 			$this->data['content'] = $this->parser->parse($this->data['pagebody'], $this->data, true);
                 
+                /*
                 // integrate any needed CSS framework & components
                 $this->data['caboose_styles'] = $this->caboose->styles();
                 $this->data['caboose_scripts'] = $this->caboose->scripts();
                 $this->data['caboose_trailings'] = $this->caboose->trailings();
+                 */
 
                 $this->parser->parse($template, $this->data);
 	}
--- application/views/template.php
+++ application/views/template.php
@@ -5,14 +5,8 @@
         <title>{pagetitle}</title>
         <meta HTTP-EQUIV="Content-Type" CONTENT="text/html; charset=UTF-8"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        {caboose_styles}
      </head>
-    <body>
-        <div class="container">
-            {navbar}
-            {content}
-        </div>
-        {caboose_scripts}
-        {caboose_trailings}
-    </body>
+     <body>
+         {content}
+     </body>
 </html>
